214d51e4d0a2004ef32051c235272af91bdb155c
vargimatixx <133201550+molnaradam0302@users.noreply.github.com>
2024-11-13 09:47:02 +0100

    groups cuccmak

--- index.js
+++ index.js
@@ -3,6 +3,8 @@ import cookieParser from "cookie-parser";
 import cors from "cors";
 import { userController } from "./controllers/user.controller.js";
 import { authController } from "./controllers/auth.controller.js";
+import { groupController } from "./controllers/group.controller.js";
+import { listAllGroup } from "./services/group.service.js"
 
 const app = express();
 
@@ -16,12 +18,21 @@ const corsOptions = {
   credentials: true,
 };
 
+app.set("view engine", "ejs");
 app.use(express.json());
 app.use(cookieParser());
 app.use(cors(corsOptions));
 
 app.use("/user", userController);
 app.use("/auth", authController);
+app.use("/group", groupController)
+
+app.get("/groups", async (req, res) => {
+  const groups = await listAllGroup()
+  res.render("groups", {
+    groups: groups
+  })
+})
 
 app.listen(3300, () => {
   console.log("http://localhost:3300");
