c4f6d742774bab155bac9aea8c81a2bfb9d34e9d
Huszka Adrián Gábor <hadrian@pollak.hu>
2024-12-02 09:13:38 +0100

    Refactor index.js to enhance CORS configuration and improve code readability

--- index.js
+++ index.js
@@ -1,54 +1,72 @@
 import express from "express";
 import cookieParser from "cookie-parser";
+import cors from "cors";
 import { userController } from "./controllers/user.controller.js";
 import { authController } from "./controllers/auth.controller.js";
 import { GetAllUsers, Groups } from "./services/user.service.js";
 import { groupController } from "./controllers/group.controller.js";
-import { listAllGroup } from "./services/group.service.js";
+import { listAllGroup } from "./services/group.service.js"
 import { listAllTokens } from "./services/auth.service.js";
 
 const app = express();
 
-app.set("view engine", "ejs");
+const corsOptions = {
+  origin: [
+    "http://localhost:5173",
+    "https://pollak.info",
+    "https://auth.pollak.info",
+    "/^https://[0-9A-Za-z]+.pollak.info$/",
+  ],
+  optionsSuccessStatus: 200,
+  credentials: true,
+};
+
+app.set("view engine", "ejs")
 app.use(express.json());
 app.use(cookieParser());
 app.use(cors(corsOptions));
-app.set("view engine", "ejs");
-app.use("/user", userController);
+app.set("view engine", "ejs")
+app.use("/user", userController)
+
+
+
 
 app.get("/", async (req, res) => {
-  res.render("index", {});
-});
+  res.render("index", {
+
+  })
+})
 
 app.get("/table", async (req, res) => {
   const userData = await GetAllUsers();
   const groupsData = await Groups();
   res.render("table", {
     users: userData,
-    groups: groupsData,
-  });
-});
+    groups: groupsData
+    
+  })
+})
 
 app.use("/user", userController);
 app.use("/auth", authController);
-app.use("/group", groupController);
+app.use("/group", groupController)
 
 app.get("/groups", async (req, res) => {
-  const groups = await listAllGroup();
+  const groups = await listAllGroup()
   res.render("groups", {
-    groups: groups,
-  });
-});
+    groups: groups
+  })
+})
 
-app.get("/", (req, res) => {
-  res.render("index");
-});
+app.get("/",(req,res) =>{
+  res.render("index")
+})
 
-app.get("/token", async (req, res) => {
+app.get("/token",async (req,res) =>{
   res.render("token", {
-    tokenData: await listAllTokens(),
-  });
-});
+    tokenData: await listAllTokens()
+  })
+})
 
 app.listen(3300, () => {
   console.log("http://localhost:3300");
